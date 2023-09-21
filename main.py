b9685eb7ada80bb9b9b60413188b6beb240ac465
priyadharsini6617 <144439434+priyadharsini6617@users.noreply.github.com>
2023-09-21 15:59:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-#write a factorial program using recursive function
-def fact_rec(n):
-  if n==0 or n==1:
-     return 1
-  else:
-    return n*fact_rec(n-1)
-number=int(input("ENTER A NUMBER:"))
-print("THE FACTORIAL OF",number,"IS:",fact_rec(number))
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
+
+
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
